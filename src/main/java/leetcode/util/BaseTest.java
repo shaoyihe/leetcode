d4b2ad776d7fd5629bdefbf99c38c22bf71e742f
he <wubuqilai@gmail.com>
2018-08-25 16:04:16 +0800

    优化

--- src/main/java/leetcode/util/BaseTest.java
+++ src/main/java/leetcode/util/BaseTest.java
@@ -18,4 +18,8 @@ public abstract class BaseTest extends Assert {
     protected <T> Set<T> newSet(List<T> objs) {
         return new HashSet<T>(objs);
     }
+
+    protected <T> void assertToSetEqual(List<T> expect, List<T> actual) {
+        assertEquals(newSet(expect), newSet(actual));
+    }
 }
