c76ca09ceccd1f178158d8f78650798c57b68083
hetiezheng <hetiezheng@jd.com>
2018-08-29 14:28:38 +0800

    410. Split Array Largest Sum

--- src/main/java/leetcode/Solution410.java
+++ src/main/java/leetcode/Solution410.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 
 /**
  * <pre>
- *  https://leetcode.com/problems/combination-sum-iv/description/
- *  377. Combination Sum IV
+ *  https://leetcode.com/problems/split-array-largest-sum/description/
+ *  410. Split Array Largest Sum
  * </pre>
  * on 2018/8/27.
  */
@@ -18,25 +18,26 @@ public class Solution410 extends BaseTest {
     }
 
     public int splitArray(int[] nums, int m) {
-        return splitArray(nums, m, 0, new int[m + 1][nums.length]);
+        return splitArray(nums, m, 0, new int[m + 1][nums.length], new int[nums.length][nums.length]);
     }
 
-    private int splitArray(int[] nums, int m, int index, int[][] cache) {
+    private int splitArray(int[] nums, int m, int index, int[][] cache, int[][] sumCache) {
         if (m == 0) return 0;
+        if (cache[m][index] > 0) return cache[m][index];
+        if (m == 1) return cache[m][index] = sum(index, nums.length - 1, nums, sumCache);
 
         int minSum = Integer.MAX_VALUE;
         for (int i = index; i <= nums.length - m; ++i) {
-            if (cache[m][i] == 0) {
-                cache[m][i] = Math.max(sum(index, i, nums), splitArray(nums, m - 1, i + 1, cache));
-            }
-            minSum = Math.min(minSum, cache[m][i]);
+            minSum = Math.min(Math.max(sum(index, i, nums, sumCache), splitArray(nums, m - 1, i + 1, cache, sumCache)), minSum);
         }
-        return minSum;
+        return cache[m][index] = minSum;
     }
 
-    private int sum(int i, int j, int[] nums) {
+    private int sum(int i, int j, int[] nums, int[][] sumCache) {
+        if (sumCache[i][j] > 0) return sumCache[i][j];
+
         int sum = 0;
         for (int t = i; t <= j; ++t) sum += nums[t];
-        return sum;
+        return sumCache[i][j] = sum;
     }
 }
