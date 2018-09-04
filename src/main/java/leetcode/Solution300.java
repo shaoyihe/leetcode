a55bd80730ef26682ea13bc2cde0c9d52e187c59
hetiezheng <hetiezheng@jd.com>
2018-09-04 10:45:52 +0800

    Longest Increasing Subsequence

--- src/main/java/leetcode/Solution300.java
+++ src/main/java/leetcode/Solution300.java
@@ -19,6 +19,11 @@ public class Solution300 extends BaseTest {
         assertEquals(4, lengthOfLIS(arr(10, 9, 2, 5, 3, 7, 101, 18)));
     }
 
+    @Test
+    public void test2() {
+        assertEquals(4, lengthOfLIS2(arr(10, 9, 2, 5, 3, 7, 101, 18)));
+    }
+
     private final int NOT_CHOICE = -1;
 
     public int lengthOfLIS(int[] nums) {
@@ -40,8 +45,23 @@ public class Solution300 extends BaseTest {
             }
             max = cache[index];
         }
-
         return Math.max(max, lengthOfLIS(nums, index + 1, cache, lastChoice));
+    }
+
+
+    public int lengthOfLIS2(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int[] cache = new int[nums.length];
+
+        int max = 1;
+        for (int i = 0; i < nums.length; ++i) {
+            int lessThanCurMaxVal = 0;
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i] && cache[j] > lessThanCurMaxVal) lessThanCurMaxVal = cache[j];
+            }
+            if ((cache[i] = lessThanCurMaxVal + 1) > max) max = cache[i];
+        }
+        return max;
     }
 
 
