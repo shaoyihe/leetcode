c4146309a472395eee0212223070a56828e507d9
hetiezheng <hetiezheng@jd.com>
2018-09-06 12:45:52 +0800

    2 Keys Keyboard

---
+++ src/main/java/leetcode/Solution650.java
@@ -0,0 +1,51 @@
+package leetcode;
+
+import leetcode.util.BaseTest;
+import org.junit.Test;
+
+/**
+ * <pre>
+ *  https://leetcode.com/problems/2-keys-keyboard/description/
+ *  650. 2 Keys Keyboard
+ * </pre>
+ * on 2018/09/03.
+ */
+public class Solution650 extends BaseTest {
+
+    @Test
+    public void test() {
+        assertEquals(3, minSteps(3));
+        assertEquals(6, minSteps(9));
+    }
+
+    public int minSteps(int n) {
+        if (n == 1) return 0;
+        int[][][] cache = new int[n + 1][n + 1][n + 1];
+        int i = 1 + minSteps(n - 1, 1, 1, cache);
+        return i;
+    }
+
+    private final int NOT_REACH = -1;
+
+    private int minSteps(int remainN, int paperN, int copyANumber, int[][][] cache) {
+        if (remainN == 0) return 0;
+        if (remainN < 0 || copyANumber > remainN) return NOT_REACH;
+
+        if (cache[copyANumber][paperN][remainN] != 0) return cache[copyANumber][paperN][remainN];
+
+        int minStep = NOT_REACH;
+        // paste
+        int nextMinStep = minSteps(remainN - copyANumber, paperN + copyANumber, copyANumber, cache);
+        if (nextMinStep != NOT_REACH) minStep = nextMinStep + 1;
+
+        if (copyANumber < paperN) {
+            // copy
+            nextMinStep = minSteps(remainN, paperN, paperN, cache);
+            if (nextMinStep != NOT_REACH) {
+                if (minStep == NOT_REACH) minStep = 1 + nextMinStep;
+                else minStep = Math.min(minStep, 1 + nextMinStep);
+            }
+        }
+        return cache[copyANumber][paperN][remainN] = minStep;
+    }
+}
