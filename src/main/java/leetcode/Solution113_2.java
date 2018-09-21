2e215927b98a8d0fc48f6492adaeaa1e254f3c90
hetiezheng <hetiezheng@jd.com>
2018-09-21 10:33:00 +0800

    113. Path Sum3

--- src/main/java/leetcode/Solution113_2.java
+++ src/main/java/leetcode/Solution113_2.java
@@ -4,26 +4,25 @@ import leetcode.util.BaseTest;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
  * <pre>
  *  https://leetcode.com/problems/path-sum/description/
- *  112. Path Sum
+ *  113. Path Sum3
  * </pre>
  * on 2018/9/19.
  */
-public class Solution113 extends BaseTest {
+public class Solution113_2 extends BaseTest {
 
     @Test
     public void test() {
         TreeNode root = new TreeNode(5);
-        TreeNode left = new TreeNode(4);
-        TreeNode right = new TreeNode(8);
-        root.left = left;
-        root.right = right;
-        System.err.println(pathSum(root, 9));
+//        TreeNode left = new TreeNode(4);
+//        TreeNode right = new TreeNode(8);
+//        root.left = left;
+//        root.right = right;
+        System.err.println(pathSum(root, 5));
     }
 
     /**
@@ -41,19 +40,17 @@ public class Solution113 extends BaseTest {
 
 
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        List<List<Integer>> result = iterPathSum(root, sum);
-        if (result != null) {
-            for (List<Integer> temp : result) Collections.reverse(temp);
-        }
+        List<List<Integer>> result = iterPathSum(root, sum, 0);
         return result == null ? new ArrayList<>() : result;
     }
 
-    private List<List<Integer>> iterPathSum(TreeNode root, int sum) {
+    private List<List<Integer>> iterPathSum(TreeNode root, int sum, int depth) {
         if (root == null) return null;
         if (root.left == null && root.right == null) {
             if (sum == root.val) {
-                List<Integer> result = new ArrayList<>();
-                result.add(root.val);
+                List<Integer> result = new ArrayList<>(depth + 1);
+                for (int i = 0; i < depth; ++i) result.add(0);
+                result.add(depth, root.val);
 
                 List<List<Integer>> finalResult = new ArrayList<>();
                 finalResult.add(result);
@@ -62,14 +59,14 @@ public class Solution113 extends BaseTest {
             return null;
         }
 
-        List<List<Integer>> left = iterPathSum(root.left, sum - root.val);
-        List<List<Integer>> right = iterPathSum(root.right, sum - root.val);
+        List<List<Integer>> left = iterPathSum(root.left, sum - root.val, depth + 1);
+        List<List<Integer>> right = iterPathSum(root.right, sum - root.val, depth + 1);
 
         if (left == null) left = right;
         else if (right != null) left.addAll(right);
 
         if (left != null) {
-            for (List<Integer> temp : left) temp.add(root.val);
+            for (List<Integer> temp : left) temp.set(depth, root.val);
         }
         return left;
     }
